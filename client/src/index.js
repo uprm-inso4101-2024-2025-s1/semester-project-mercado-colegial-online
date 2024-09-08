29873025b7c66055f492b83a1130ddaa94665c4d
Fabiola Z. Torres Maldonado <fabiola.torres28@upr.edu>
2024-09-08 13:19:54 -0400

    Added form validation to ensure users cannot be created if all fields are not filled

--- client/src/index.js
+++ client/src/index.js
@@ -87,22 +87,42 @@ document.getElementById("mySubmit").onclick = function() {      //takes user inp
     phone = document.getElementById("myPhone").value;
     password = document.getElementById("myPassword").value;
 
-    // Create a new User instance after the button is clicked
-    const user1 = new User(fullName, username, email, phone, password);
-    user1.displayInfo();
-    userArray.push(user1);                                      //Adds user1 to userArray
-    console.log('Users Array', {userArray});                    
-    fetch('http://localhost:3000/login', {
-        method: 'POST',
-        body: JSON.stringify(user1.json()),
-        headers: { 'Content-Type': 'application/json' }
-    }).then(response => response.json()).then(data => {
-        console.log(data);
-        const messageElement = document.createElement('p');
-        messageElement.innerHTML = data.message;
-        document.body.appendChild(messageElement);
-    });
+    // Validate that all required fields were filled
+    if(fullName!="" && username!="" && email!="" && phone!="" && password!=""){
 
-    //Saving to local storage
-   localStorage.setItem('LocalUsersArray',JSON.stringify(userArray));
+        // Create a new User instance after the button is clicked
+        const user1 = new User(fullName, username, email, phone, password);
+        user1.displayInfo();
+        userArray.push(user1);                                      //Adds user1 to userArray
+        console.log('Users Array', {userArray});                    
+        fetch('http://localhost:3000/login', {
+            method: 'POST',
+            body: JSON.stringify(user1.json()),
+            headers: { 'Content-Type': 'application/json' }
+        }).then(response => response.json()).then(data => {
+            console.log(data);
+            const messageElement = document.createElement('p');
+            messageElement.innerHTML = data.message;
+            document.body.appendChild(messageElement);
+        });
+
+        //Saving to local storage
+        localStorage.setItem('LocalUsersArray',JSON.stringify(userArray));
+
+        if(document.getElementById("errorMessage")){
+            document.getElementById("errorMessage").remove()
+        }
+        
+    } else{
+
+        // If all fields were not filled, error message is displayed and no user is created
+        console.log("Required sing up information is missing")
+        const errorMessage = document.createElement('p');
+        errorMessage.id = "errorMessage"
+        errorMessage.innerHTML = "Please fill in all fields to continue";
+        if(!document.getElementById("errorMessage")){
+            document.body.appendChild(errorMessage);
+        }
+        
+    }
 };
