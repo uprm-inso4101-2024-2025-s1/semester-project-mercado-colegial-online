171187de0caee4027fc19e1c6fda352aec78199f
CdoxHype <julio.camacho3@upr.edu>
2024-09-13 13:18:39 -0300

    Save untracked files before applying stash

---
+++ vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+body {
+  background-color: white; 
+  color: black; /* Ensure text is visible */
+  font-family: Arial, sans-serif;
+}
+</style>
