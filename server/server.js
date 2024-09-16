285a200acb8424def833621bb4458644ae06079c
diegoVazquez7 <diego.vazquez7@upr.edu>
2024-09-16 12:14:30 -0500

    password hashing and salting (server-side)

--- server/server.js
+++ server/server.js
@@ -2,15 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const crypto = require('crypto');
+
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
 
+function hashPassword(password, salt){
+    return crypto.pbkdf2Sync(password,salt,1000,64,'sha512').toString('hex');
+}
+function generateSalt(){
+    return crypto.randomBytes(16).toString('hex');
+}
+
 app.post('/signup', (req, res) => {
     const receivedData = req.body;
-    console.log('Sign Up data received from client:', receivedData);
+
+    const { password } = receivedData;
+    if(!password){
+        return res.status(400).json({message: 'Password is required'});
+    }
+    const salt = generateSalt();
+    const hashedPass = hashPassword(password,salt);
 
     res.json({ message: 'Signed Up successfully', data: receivedData });
 });
