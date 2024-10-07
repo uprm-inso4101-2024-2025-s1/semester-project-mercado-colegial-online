f60506860c56d9b8bda3523e3db4d88b566a6527
JuanDelPueblo <edyancruz@outlook.com>
2024-10-06 21:01:54 -0400

    Connect database to authentication and read/write user data from it

--- vue-project/server/server.js
+++ vue-project/server/server.js
@@ -11,9 +11,6 @@ import dotenv from 'dotenv';
 const app = express();
 const port = 3000;
 
-/** For temporary stored user data */
-const users = [];   
-
 /** Middleware  */
 
 /** Parses incoming JSON request bodies to make it easier to handle data */
@@ -42,8 +39,8 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 // dotenv.config(); 
 // const uri = process.env.MONGODB_URI; 
 
-const uri = "mongodb://mercadocolegial:M.Colegio9@cluster0-shard-00-00.vcnew.mongodb.net:27017,cluster0-shard-00-01.vcnew.mongodb.net:27017,cluster0-shard-00-02.vcnew.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
-
+// const uri = "mongodb://mercadocolegial:M.Colegio9@cluster0-shard-00-00.vcnew.mongodb.net:27017,cluster0-shard-00-01.vcnew.mongodb.net:27017,cluster0-shard-00-02.vcnew.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+const uri = "mongodb+srv://mercadocolegial:M.Colegio9@cluster0.vcnew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -71,6 +68,30 @@ async function run() {
 }
 run().catch(console.dir);
 
+async function addUser(newUser) {
+    try {
+        await client.connect();
+        const database = client.db("sample_mflix");
+        const users = database.collection("users");
+        const result = await users.insertOne(newUser);
+        console.log(`New user created with the following id: ${result.insertedId}`);
+    } finally {
+        await client.close();
+    }
+}
+
+async function getUser(username) {
+    try {
+        await client.connect();
+        const database = client.db("sample_mflix");
+        const users = database.collection("users");
+        const user = await users.findOne({ username: username });
+        await client.close();
+        return user;
+    } catch (e) {
+        console.error(e);
+    }
+}
 
 /** Signup Route */
 
@@ -96,9 +117,11 @@ app.post('/signup', (req, res) => {
     console.log(`Hashed Password: ${hashedPass}`);
     console.log(`Salt: ${salt}`);
 
-    users.push({ ...receivedData, password: hashedPass, salt });
-
-    res.json({ message: 'Signed Up successfully', data: receivedData });
+    addUser({ ...receivedData, password: hashedPass, salt }).then(() => {
+      res.json({ message: 'Signed Up successfully', data: receivedData });
+    }).catch((e) => {
+      res.status(500).json({ message: 'Failed to sign up', error: e });
+    });
 });
 
 
@@ -116,24 +139,27 @@ app.post('/login', (req, res) => {
 
     const { password } = receivedData;
 
-    const user = users.find(user => user.username === receivedData.username);
+    getUser(receivedData.username).then((user) => {
 
-    if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-    }
+      if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+      }
 
-    const hashedPass = hashPassword(password,user.salt);
+      const hashedPass = hashPassword(password,user.salt);
 
-    console.log(user);
-    console.log(`Original Password: ${password}`);
-    console.log(`Hashed Password: ${hashedPass}`);
-    console.log(`Salt: ${user.salt}`);
+      console.log(user);
+      console.log(`Original Password: ${password}`);
+      console.log(`Hashed Password: ${hashedPass}`);
+      console.log(`Salt: ${user.salt}`);
 
-    if (user.password !== hashedPass) {
-        return res.status(401).json({ message: 'Invalid credentials' });
-    }
+      if (user.password !== hashedPass) {
+          return res.status(401).json({ message: 'Invalid credentials' });
+      }
 
-    res.json({ message: 'Logged In successfully', data: receivedData });
+      res.json({ message: 'Logged In successfully', data: receivedData });
+    }).catch((e) => {
+      res.status(500).json({ message: 'Failed to login', error: e });
+    });
 })
 
 /** Starting the Server */
