0ab2b80b02aba9a95821d95bb087dbc1e0790d5b
franRov13 <francisco.a.rovira1@gmail.com>
2024-10-20 23:23:21 -0400

    fixed minor error within item model

--- vue-project/server/backend/models/item.js
+++ vue-project/server/backend/models/item.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const itemSchema = new mongoose.Schema({
 
   item_id: {
-    type: mongoose.Schema.Types.ObjectId, 
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(), 
     index: true,
     unique: true,
   },
