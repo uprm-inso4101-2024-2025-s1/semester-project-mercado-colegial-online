37ef82610cfb5ebff067d60dde55166a647daa02
F3rnand0hr <fernando.hidalgo@upr.edu>
2024-10-22 21:37:12 -0400

    finished email confirmation email file

--- vue-project/src/EmailConfirmation.js
+++ vue-project/src/EmailConfirmation.js
@@ -13,10 +13,31 @@ const sendEmail = async (userEmail, userId) => {
 
     const confirmURL = `http://localhost:5173/confirm/${token}`;
 
+    const sender = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+    });
 
+const mailOptions = {
+    from: '"Mercado Colegial Colegial" <mercado.colegial802@gmail.com>',
+    to: userEmail,
+    subject: 'Confirmación de Registro - Cuenta de Mercado Colegial Online',
+    html: `
+      <h1>Bienvenido al Mercado Colegial Online!</h1>
+      <p>Gracias por registrarte. Por favor confirma tu cuenta haciendo clic en el enlace de abajo:</p>
+      <a href="${confirmationUrl}">Confirmar mi cuenta</a>
+      <p>Este enlace expira en ? horas.</p> 
+    `,
+  };    
 
-
-    //le falta a la funcion ! ! !
-
+try {
+    await sender.sendMail(mailOptions);
+    console.log('Correo enviado');
+}   catch (error){
+    console.log('Error al enviar el correo', error);
+}
 
 };
